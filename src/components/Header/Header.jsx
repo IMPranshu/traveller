cbaef7dc251534df9c08b5a76c169c5c55e098a9
P K Agrawal <pranshukumar1999@gmail.com>
2022-03-17 10:50:46 +0530

    Header layout done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, ToolBar, Typography, InputBase, Box} from '@material-ui/core';
+import { classes } from "istanbul-lib-coverage";
+import SearchIcon from '@material-ui/icons';
+
 
 const Header = () => {
   return (
 
-    <h1>Header</h1>
+    <AppBar position="static">
+      <ToolBar className={classes.toolBar}>
+        <Typography variant="h5" className={classes.title}>
+          Traveller
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h5" className={classes.title}>
+            Explore every place
+          </Typography>
+          <Autocomplete>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search..." classes={{root : classes.inputRoot, input: classes.inputInput }} />
+
+            </div>
+          </Autocomplete>
+        </Box>
+      </ToolBar>
+    </AppBar>
   );
 }
 
